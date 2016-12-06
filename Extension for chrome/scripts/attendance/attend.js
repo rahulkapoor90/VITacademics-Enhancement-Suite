41b24cf3d1d7f98195b981d8046d639bad827ad9
VaTz88 <vatz88@gmail.com>
2016-12-06 18:48:58 +0530

    Attendance page done.

--- Extension for chrome/scripts/attendance/attend.js
+++ Extension for chrome/scripts/attendance/attend.js
@@ -1,27 +1,26 @@
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('calc.js');
-(document.head||document.documentElement).appendChild(s);
-s.onload = function() {
+(document.head || document.documentElement).appendChild(s);
+s.onload = function () {
     s.parentNode.removeChild(s);
 };
 
-function fixVITCode(){
+function fixVITCode() {
     var d = document.getElementsByTagName("table")[4];
     d = d.getElementsByTagName('tbody')[0]
     d = d.getElementsByTagName('tr');
     console.log(d);
-    for(var i=1; i<d.length; i++){
+    for (var i = 1; i < d.length; i++) {
         var formTag = d[i].getElementsByTagName('form')[0];
         temp = formTag.outerHTML.split('><');
-        temp['0']+='>';
-        temp['1'] = '<'+temp['1'];
+        temp['0'] += '>';
+        temp['1'] = '<' + temp['1'];
         var viewCol = d[i].getElementsByTagName('td')[10];
-        viewCol.innerHTML = temp['0']+viewCol.innerHTML+temp['1'];
+        viewCol.innerHTML = temp['0'] + viewCol.innerHTML + temp['1'];
     }
 }
 
-function init()
-{
+function init() {
     fixVITCode();
     var d = document.getElementsByTagName("table")[4];
     d = d.getElementsByTagName('tr');
@@ -31,28 +30,31 @@ function init()
         e.value = "+";
         e.innerHTML = "+";
         e.setAttribute('onclick', "calc(3," + i + ")");
+        e.setAttribute('class', "btn-floating btn waves-effect waves-light green");
 
         var h = document.createElement("button");
         h.value = "-";
         h.innerHTML = "-";
         h.setAttribute('onclick', "calc(4," + i + ")");
+        h.setAttribute('class', "btn-floating btn waves-effect waves-light red");
 
         var g = document.createElement("button");
         g.value = "+";
         g.innerHTML = "+";
         g.setAttribute('onclick', "calc(1," + i + ")");
+        g.setAttribute('class', "btn-floating btn waves-effect waves-light green");
 
         var f = document.createElement("button");
         f.value = "-";
         f.innerHTML = "-";
         f.setAttribute("onclick", "calc(2," + i + ")");
+        f.setAttribute('class', "btn-floating btn waves-effect waves-light red");
 
         d[i].innerHTML += d[i].getElementsByTagName("td")[10].outerHTML;
         if (i == 0) {
             d[i].getElementsByTagName("td")[11].childNodes[0].childNodes[0].textContent = 'Check Your Attendance';
-        }
-        else {
-            d[i].getElementsByTagName("td")[11].innerHTML =  e.outerHTML+h.outerHTML+"<p id='at"+i+"'>Attend:0</p>"+ g.outerHTML+f.outerHTML+ "<p id='mi"+i+"'>Miss: 0</p>";
+        } else {
+            d[i].getElementsByTagName("td")[11].innerHTML = "<div class='AttendDiv'>" + h.outerHTML + "<span id='at" + i + "' class='AttendMissLabel'>Attend:0</span>" + e.outerHTML + "</div><div>" + f.outerHTML + "<span id='mi" + i + "' class='AttendMissLabel'>Miss: 0</span>" + g.outerHTML + "</div>";
             d[i].getElementsByTagName("td")[6].innerHTML = "<p id='ao" + i + "'>" + d[i].getElementsByTagName("td")[6].innerHTML + "</p><p id='a" + i + "'>" + d[i].getElementsByTagName("td")[6].innerHTML;
             d[i].getElementsByTagName("td")[8].innerHTML = "<p id='po" + i + "'>" + d[i].getElementsByTagName("td")[8].innerHTML + "</p>" + "<p id='p" + i + "'>" + d[i].getElementsByTagName("td")[8].innerHTML + "</p>";
             d[i].getElementsByTagName("td")[7].innerHTML = "<p id='tt" + i + "'>" + d[i].getElementsByTagName("td")[7].innerHTML + "</p>" + "<p id='t" + i + "'>" + d[i].getElementsByTagName("td")[7].innerHTML + "</p>";
--- Extension for chrome/scripts/attendance/attendance.js
+++ Extension for chrome/scripts/attendance/attendance.js
@@ -1,25 +1,158 @@
-function getDateString(date) {
-	var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-	return ("0"+date.getDate()).slice(-2)
-			+"-"+
-			month[date.getMonth()]
-			+"-"+
-			date.getFullYear();
-}
-document.getElementsByName("to_date")[0].value=getDateString(new Date());
-$(document).ready(function () {
-	//$('table:first-child ul:first-child').hide();
-   //     $('table:nth-child(1) td:nth-child(7)').hide();
-});
-$(document).ready(function () {
-	var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/blue/pace-theme-flat-top.css';
-    document.getElementsByTagName('head')[0].appendChild(link);
-    var script1 = document.createElement('script');
-    script1.src = 'https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.js';
-    document.getElementsByTagName('head')[0].appendChild(script1);
-    
-});
+// $(function () {
+// 	//remove remove default css file
+// 	$('link[href="style.css"]').remove();
+// 	$('link[href="style2.css"]').remove();
 
-document.getElementsByName('from_date')[0].value = '01-JAN-2016'
+// 	// remove inline attributes
+// 	$('*[style]').removeAttr('style');
+// 	$('*').removeAttr('cellspacing');
+// 	$('*').removeAttr('cellpadding');
+// 	$('*').removeAttr('width');
+// 	$('*').removeAttr('height');
+// 	$('*').removeAttr('bordercolor');
+// 	$('*').removeAttr('bgcolor');
+// 	$('*').removeAttr('border');
+// 	$('*').removeAttr('align');
+// 	$('*').removeAttr('valign');
+// 	$('font').contents().unwrap();
+// 	$('u').contents().unwrap();
+// 	$('br').remove();
+
+// 	$('table').eq(0).replaceWith('<div class="container-fluid">' + $('table').eq(0).html() + '</div>');
+
+// 	$('center').eq(0).replaceWith('<h5>' + $('center').eq(0).text() + '</h5>');
+
+// 	// attendance date range form
+// 	while ($('form table').length > 0) {
+// 		$('form table').eq(0).replaceWith('<div>' + $('form table').html() + '</div>');
+// 	}
+// 	$('form').eq(0).addClass('card-panel hoverable');
+// 	$('form b').remove();
+// 	$('form').eq(0).find('a').remove();
+// 	$('form img').remove();
+// 	//from date 32 ; to date 33
+// 	$('#demo32').attr('type', 'date');
+// 	$('#demo32').removeAttr('readonly');
+// 	$('#demo32').addClass('datepicker');
+// 	$('#demo33').attr('type', 'date');
+// 	$('#demo33').removeAttr('readonly');
+// 	$('#demo33').addClass('datepicker');
+// 	$('#demo32').attr('placeholder', 'Select from date');
+// 	$('#demo33').attr('placeholder', 'Select to date');
+
+// 	$('form').eq(0).prepend('<b>CGPA<b>');
+
+// 	while ($('table').eq(2).find('td').length > 0) {
+// 		$('table').eq(2).find('td').eq(0).replaceWith('<div class="col s1 center-align"><td>' + $('table').eq(2).find('td').eq(0).html() + '</td></div>');
+// 	}
+// 	while ($('table').eq(2).find('tr').length > 0) {
+// 		$('table').eq(2).find('tr').eq(0).replaceWith('<div class="card-panel hoverable"><div class="row">' + $('table').eq(2).find('tr').eq(0).html() + '</div></div>');
+// 	}
+// 	while ($('table').length > 2) {
+// 		$('table').eq(2).replaceWith('<div>' + $('table').eq(2).html() + '</div>');
+// 	}
+// 	$('.col:first-child').remove(); //Sr. no.
+// 	$('.col:nth-child(2)').addClass('boldCol'); //course title
+// 	// $('.col:nth-child(4)').addClass('s2'); // slot
+// 	$('.col:nth-child(8)').append('%');
+// 	$('.col:nth-child(8)').addClass('boldCol');
+// 	$('.col:nth-child(5)').remove(); //Reg. Date
+
+// 	$("input[type='submit']").addClass('btn col');
+// 	$('.col:last-child').addClass('right');
+// 	var i = $(".btn").parent().length - 1;
+// 	while (i >= 0) {
+// 		i--;
+// 		$(".btn").parent().eq(i).replaceWith('<form method="post" action="attn_report_details.asp">' + $(".btn").parent().eq(i).html() + '</form>');
+// 	}
+// 	$(".submit3").addClass('btn');
+
+// 	$('table').addClass(function () {
+// 		return 'centered hoverable highlight z-depth-1';
+// 	});
+
+// 	// datepicker initialization
+// 	$('.datepicker').pickadate({
+// 		selectMonths: true, // Creates a dropdown to control month
+// 		selectYears: 5 // Creates a dropdown of 15 years to control year
+// 	});
+// 	// set default date
+// 	var $input = $('#demo32').pickadate();
+// 	var picker = $input.pickadate('picker');
+// 	picker.set('select', [2016, 06, 11]);
+// 	$input = $('#demo33').pickadate();
+// 	picker = $input.pickadate('picker');
+// 	picker.set('select', [2016, 10, 11]);
+// });
+
+$(function () {
+	//remove remove default css file
+	$('link[href="style.css"]').remove();
+	$('link[href="style2.css"]').remove();
+
+	// remove inline attributes
+	$('*[style]').removeAttr('style');
+	$('*').removeAttr('cellspacing');
+	$('*').removeAttr('cellpadding');
+	$('*').removeAttr('width');
+	$('*').removeAttr('height');
+	$('*').removeAttr('bordercolor');
+	$('*').removeAttr('bgcolor');
+	$('*').removeAttr('border');
+	$('*').removeAttr('align');
+	$('*').removeAttr('valign');
+	$('*').removeAttr('onmouseover');
+	$('*').removeAttr('onmouseout');
+	$('font').contents().unwrap();
+	$('u').contents().unwrap();
+	// $('br').remove();
+
+	// attendance date range form
+	while ($('form table').length > 0) {
+		$('form table').eq(0).replaceWith('<div>' + $('form table').html() + '</div>');
+	}
+	$('form').eq(0).addClass('card-panel hoverable');
+	$('form b').remove();
+	$('form').eq(0).find('a').remove();
+	$('form img').remove();
+	//from date 32 ; to date 33
+	$('#demo32').attr('type', 'date');
+	$('#demo32').removeAttr('readonly');
+	$('#demo32').addClass('datepicker');
+	$('#demo33').attr('type', 'date');
+	$('#demo33').removeAttr('readonly');
+	$('#demo33').addClass('datepicker');
+	$('#demo32').attr('placeholder', 'Select from date');
+	$('#demo33').attr('placeholder', 'Select to date');
+
+	$('form').eq(0).prepend('<b>CGPA<b>');
+
+	$('table').eq(0).replaceWith('<div class="container-fluid">' + $('table').eq(0).html() + '</div>');
+
+	$('table').addClass(function () {
+		return 'centered hoverable highlight z-depth-1';
+	});
+
+	while ($('b').length > 0) {
+		$('b').eq(0).replaceWith('<div style="font-weight:bold;">' + $('b').eq(0).text() + '</div>');
+	}
+	$('center').eq(0).replaceWith('<h5>' + $('center').eq(0).text() + '</h5>');
+
+	$(".submit3").addClass('btn');
+	$("input[type='submit']").addClass('btn');
+
+	// datepicker initialization
+	$('.datepicker').pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 5 // Creates a dropdown of 15 years to control year
+	});
+	// set default date
+	var $input = $('#demo32').pickadate();
+	var picker = $input.pickadate('picker');
+	picker.set('select', [2016, 06, 11]);
+	$input = $('#demo33').pickadate();
+	picker = $input.pickadate('picker');
+	picker.set('select', [2016, 10, 11]);
+
+	$('button').addClass('btn-floating btn');
+});
