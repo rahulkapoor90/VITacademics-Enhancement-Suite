bcce4193d4fd356d868b5aabe2c42aa6963756dd
VaTz88 <vatz88@gmail.com>
2016-12-09 12:37:20 +0530

    vtop doodle update dynamically

--- Extension for chrome/scripts/stud_login/stud_login.js
+++ Extension for chrome/scripts/stud_login/stud_login.js
@@ -706,11 +706,12 @@ $(function () {
 
     }
 
-    
+
     $("link[href='style.css']").remove(); //remove remove default css file
     $("link[href='style2.css']").remove();
     $("body").addClass("container");
     $('body').load(chrome.extension.getURL("scripts/stud_login/stud_login.html"), function () {
+        $('#doodle-text').load('https://vtop-doodle.azurewebsites.net/doodle.txt'); // get doodle txt
         loaded();
     }); //Content inside body tag will be replaced by content in stud_login.html
 });
