2828f0578ab0d92461d5cb5b1de9c78b2cdcffbf
VaTz88 <vatz88@gmail.com>
2016-12-30 16:53:32 +0530

    different names for downloaded images from timetable page

--- Extension for chrome/scripts/timeTable/timetable.js
+++ Extension for chrome/scripts/timeTable/timetable.js
@@ -1,36 +1,36 @@
+function table2jpg(element, filename) {
+    var newWindow = window.open();
+    html2canvas(element, {
+        onrendered: function (canvas) {
+            timetable_img_src = canvas.toDataURL("image/jpeg");
+            newWindow_data =
+                '<html><head><title>VES</title></head><body><a href="' + timetable_img_src + '" download="' + filename + '"><img style="width:100%;" src="' + timetable_img_src + '" alt="FFCSonTheGo"/></a>' +
+                '<h1>Click on the image to download.</h1>';
+
+            newWindow.document.write(newWindow_data);
+        }
+    });
+}
+
 $(function () {
 
-    function table2jpg(element) {
-        var newWindow = window.open();
-        html2canvas(element, {
-            onrendered: function (canvas) {
-                timetable_img_src = canvas.toDataURL("image/jpeg");
-                newWindow_data =
-                    '<html><head><title>VES</title></head><body><a href="' + timetable_img_src + '" download="VES_MyTimeTable"><img style="width:100%;" src="' + timetable_img_src + '" alt="FFCSonTheGo"/></a>' +
-                    '<h1>Click on the image to download.</h1>';
-
-                newWindow.document.write(newWindow_data);
-            }
-        });
-    }
-
+    // create buttons
     var tableOne_btn = $('<input type="button" value="Export to JPG">');
     var tableTwo_btn = $('<input type="button" value="Export to JPG">');
-
-    var tableOneElement = $('table')[1];
-    console.log(tableOneElement);
-    var tableTwoElement = $('table')[2];
-    console.log(tableTwoElement);
-
-    tableOne_btn.click(function () {
-        table2jpg(tableOneElement);
-    });
-
-    tableTwo_btn.click(function () {
-        table2jpg(tableTwoElement);
-    });
 
     // add buttons
     $('table').eq(1).after(tableOne_btn);
     $('table').eq(2).after(tableTwo_btn);
+
+    // get table elements
+    var tableOneElement = $('table')[1];
+    var tableTwoElement = $('table')[2];
+
+    // add on click event listener
+    tableOne_btn.click(function () {
+        table2jpg(tableOneElement, "MyCourses");
+    });
+    tableTwo_btn.click(function () {
+        table2jpg(tableTwoElement, "MyTimetable");
+    });
 });
